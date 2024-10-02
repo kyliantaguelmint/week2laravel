99b5aa50dd7133603f9ed76079dd39fcf66301f3
ktaguelmint <kylian.taguelmint@epitech.eu>
2024-10-02 03:40:53 +0200

    week5

--- my-laravel-app/app/Models/Post.php
+++ my-laravel-app/app/Models/Post.php
@@ -1,11 +1,13 @@
 <?php
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
 class Post  extends Model
 {
+    use HasFactory;
     protected $table = 'posts';
     protected $primaryKey = 'id';
     protected $fillable = ['body','title','author'];
